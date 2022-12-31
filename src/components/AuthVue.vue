6834bc80e34d61d92358e5d77927ecb71ad0b42b
VernoB <mbagna3@gmail.com>
2022-12-31 21:01:52 +0400

    adding default values and multiple errors behaviors

--- src/components/AuthVue.vue
+++ src/components/AuthVue.vue
@@ -90,7 +90,12 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <vee-form v-show="tab === 'register'" :validation-schema="schema">
+          <vee-form
+            v-show="tab === 'register'"
+            :validation-schema="schema"
+            @submit="register"
+            :initial-values="formValues"
+          >
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -128,12 +133,21 @@
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
               <vee-field
-                type="password"
                 name="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
-              <ErrorMessage class="text-red-600" name="password" />
+                :bails="false"
+                v-slot="{ field, errors }"
+              >
+                <input
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Password"
+                  type="password"
+                  v-bind="field"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+              </vee-field>
+              <!-- <ErrorMessage class="text-red-600" name="password" /> -->
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
@@ -198,9 +212,17 @@ const schema = {
   name: "required|min:3|max:100|alpha_spaces|alpha",
   email: "required|min:5|max:100|email",
   age: "required|alpha_num|min_value:18|max_value:100|numeric",
-  password: "required|min:3|max:100|alpha_dash",
+  password: "required|min:8|max:100|excluded:password",
   confirmPassword: "required|confirmed:@password",
   country: "required|excluded:Antartica",
   tos: "required",
+};
+
+const register = (values) => {
+  console.log(values);
+};
+
+const formValues = {
+  country: "USA",
 };
 </script>
